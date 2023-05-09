cf2e4f8ee1950e160eb5f50a5265803eeecf9bce
Mikhail <mich_p@inbox.ru>
2023-05-09 16:30:27 +0300

    Initial Commit

---
+++ SmartwayGalery/SmartwayGaleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartwayGaleryApp.swift
+//  SmartwayGalery
+//
+//  Created by Миша Перевозчиков on 09.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SmartwayGaleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

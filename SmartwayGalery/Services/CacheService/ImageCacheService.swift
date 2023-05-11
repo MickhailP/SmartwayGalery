124710fb8a9a2fb59add43ddb453f1b8d81ec9ad
Mikhail <mich_p@inbox.ru>
2023-05-11 11:31:24 +0300

    Добавлена очистка кэша после завершения работы приложения

--- SmartwayGalery/Services/CacheService/ImageCacheService.swift
+++ SmartwayGalery/Services/CacheService/ImageCacheService.swift
@@ -13,6 +13,7 @@ final class ImageCacheService {
 	static let shared = ImageCacheService()
 	private init () { }
 
+
 	var photoCache: NSCache<NSString, UIImage> = {
 		var cache = NSCache<NSString, UIImage>()
 		cache.countLimit = 200
@@ -20,12 +21,20 @@ final class ImageCacheService {
 		return cache
 	}()
 
+
 	func add(key: String, value: UIImage) {
 		photoCache.setObject(value, forKey: key as NSString)
 	}
 
+
 	func get(key: String) -> UIImage? {
 		photoCache.object(forKey: key as NSString)
+	}
+
+
+	func clearCache() {
+		photoCache.removeAllObjects()
+		print("cache cleared")
 	}
 }
 
--- SmartwayGalery/SmartwayGaleryApp.swift
+++ SmartwayGalery/SmartwayGaleryApp.swift
@@ -7,11 +7,22 @@
 
 import SwiftUI
 
+
 @main
 struct SmartwayGaleryApp: App {
+
+	@UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
 			GalleryView()
         }
     }
+}
+
+
+@objc class AppDelegate: NSObject, UIApplicationDelegate {
+	func applicationWillTerminate(_ application: UIApplication) {
+		ImageCacheService.shared.clearCache()
+	}
 }
